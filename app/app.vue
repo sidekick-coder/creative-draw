54ce2cc72a0a2063d221d0ce11cda535c3e9252d
Henrique de Oliveira <henriqueoliwork@gmail.com>
2024-11-22 11:18:31 -0300

    feat: initial setup

---
+++ app/app.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex h-screen w-screen bg-zinc-800 text-white">
+        <div class="w-72 bg-zinc-700">sidebar</div>
+        <div class="flex-1 p-5">
+            <d-canvas />
+        </div>
+    </div>
+</template>
---
+++ app/components/DCanvas.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+const canvas = ref<HTMLCanvasElement>()
+let context: CanvasRenderingContext2D
+
+const state = reactive({
+    offsetX: 0,
+    offsetY: 0,
+    lastX: 0,
+    lastY: 0,
+})
+
+const isDrawing = ref(false)
+
+function onMouseMove(e: MouseEvent) {
+    if (!isDrawing.value) return
+
+    context.lineWidth = 10
+    context.lineCap = 'round'
+    context.lineJoin = 'round'
+
+    context.beginPath()
+
+    context.moveTo(state.lastX, state.lastY)
+    context.lineTo(e.clientX - state.offsetX, e.clientY)
+
+    context.stroke()
+
+    state.lastX = e.clientX - state.offsetX
+    state.lastY = e.clientY
+}
+
+function load() {
+    if (!canvas.value) return
+
+    const parent = canvas.value.parentElement!
+
+    state.offsetX = canvas.value.offsetLeft
+    state.offsetY = canvas.value.offsetTop
+
+    canvas.value.width = parent.clientWidth - state.offsetX
+    canvas.value.height = parent.clientHeight - state.offsetY
+
+    context = canvas.value.getContext('2d')!
+
+    context.strokeStyle = '#000000'
+
+    canvas.value.addEventListener('mousedown', (e) => {
+        isDrawing.value = true
+        state.lastX = e.offsetX
+        state.lastY = e.offsetY
+    })
+
+    canvas.value.addEventListener('mouseup', () => {
+        isDrawing.value = false
+    })
+
+    canvas.value.addEventListener('mousemove', onMouseMove)
+}
+
+onMounted(load)
+</script>
+
+<template>
+    <canvas ref="canvas" class="bg-white" />
+</template>
