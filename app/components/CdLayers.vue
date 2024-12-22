1d50802169c6a53a502674ac3e4fc97321756147
Henrique <henrique@sidekick-coder.com>
2024-12-22 19:28:25 +0000

    refactor: remove old components

--- app/components/CdLayers.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-const instance = useInstance()
-
-const layers = computed(() => instance.layers.value)
-</script>
-
-<template>
-    <cd-canvas2 v-for="l in layers" :key="l.id" :layer="l" />
-</template>
