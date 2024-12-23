06eae6d22afdb7db681d7d7a36de07860a802e89
Henrique <henrique@sidekick-coder.com>
2024-12-23 20:16:52 +0000

    feat: add fallback create id

--- app/composables/createId.ts
+++ app/composables/createId.ts
@@ -1,11 +1,14 @@
+function fallback() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c => (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16));
+}
 export function createId() {
     if (import.meta.server) {
         return require('crypto').randomUUID()
     }
 
-    if (window.crypto.randomUUID) {
+    if (typeof window.crypto.randomUUID === 'function') {
         return window.crypto.randomUUID()
     }
 
-     return useId()
+    return fallback()
 }
