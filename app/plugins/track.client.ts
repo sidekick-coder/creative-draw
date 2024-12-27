c8623e93fa2b88fa598bd65db1e96dc9c4df3182
Henrique <henrique@sidekick-coder.com>
2024-12-27 19:20:03 +0000

    feat: track pageleave

--- app/plugins/track.client.ts
+++ app/plugins/track.client.ts
@@ -3,8 +3,14 @@ import { defineNuxtPlugin } from '#app'
 export default defineNuxtPlugin(() => {
     const router = useRouter()
 
-    router.afterEach((to) => {
+    router.afterEach((to, from) => {
         nextTick(() => {
+            if (from.fullPath !== to.fullPath) {
+                $track('$pageleave', {
+                    current_url: from.fullPath,
+                })
+            }
+
             $track('$pageview', {
                 current_url: to.fullPath,
             })
