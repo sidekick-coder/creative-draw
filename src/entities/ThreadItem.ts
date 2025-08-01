71e94a1658edd253ed2e273afacea12a68d9619b
Henrique <henrique@sidekick-coder.com>
2025-07-31 21:21:33 -0300

    refactor: add order

--- src/entities/ThreadItem.ts
+++ src/entities/ThreadItem.ts
@@ -1,26 +1,25 @@
 import type { Instruction } from '@/contracts/AdapterRunnerGateway'
 
 export default class ThreadItem {
-    constructor(
-        public id: string,
-        public threadId: string,
-        public type: 'text' | 'image',
-        public data: Record<string, any>,
-        public createdAt: Date,
-        public updatedAt: Date,
-        public deletedAt: Date | null = null
-    ) {}
+    public id: string
+    public threadId: string
+    public type: 'text' | 'image' | 'gallery'
+    public data: Record<string, any>
+    public createdAt: Date
+    public updatedAt: Date
+    public deletedAt: Date | null = null
+    public order: number = 999
 
-    public static fromData(data: Partial<ThreadItem>): ThreadItem {
-        return new ThreadItem(
-            data.id || '',
-            data.threadId || '',
-            data.type || 'text',
-            data.data || {},
-            new Date(data.createdAt || Date.now()),
-            new Date(data.updatedAt || Date.now()),
-            data.deletedAt ? new Date(data.deletedAt) : null
-        )
+    constructor(data?: Partial<ThreadItem>) {
+        Object.assign(this, data)
+
+        if (!this.id) {
+            this.id = createId()
+        }
+    }
+
+    public static fromData(data: any): ThreadItem {
+        return new ThreadItem(data)
     }
 
     public toInstruction(): Instruction[] {
